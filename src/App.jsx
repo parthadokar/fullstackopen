494d016a3c96ba10431e399c3afa4257fa4a7c62
parthadokar <adokar.parth@gmail.com>
2025-12-24 22:04:55 +0530

    excersise 1.5 done

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum,part) => sum + part.exercises,0)
+  return (
+    <p>Number of exercises {total}</p>    
+  )
+}
+
+const Part = ({name,exercises}) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
