45ed8594f62c13a2889a0dafe1b3cf832867ff87
parthadokar <adokar.parth@gmail.com>
2025-12-25 22:44:59 +0530

    2.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,58 @@
-import { useState } from 'react'
+import Course from "./Components/Course"
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [votes,setVotes] = useState(new Array(anecdotes.length).fill(0))
-  const nextAnecdote = () => {
-    const random = Math.floor(Math.random() * anecdotes.length)
-    setSelected(random)
-  }
-
-  const voteAnecdote = () => {
-    const copy = [...votes]
-    copy[selected] += 1
-    setVotes(copy)
-  }
-
-  const mostVotes = Math.max(...votes)
-  const topAnecdoteIndex = votes.indexOf(mostVotes)
-  return (
-      <div>
-        <h2>Anecdote of the day</h2>
-        <p>{anecdotes[selected]}</p>
-        <p>Has {votes[selected]} votes</p>
-
-        <button onClick={voteAnecdote}>vote</button>
-        <button onClick={nextAnecdote}>next anecdote</button>
-        <h2>Anecdote with most votes</h2>
-        {mostVotes === 0 
-          ? <p>No votes yet</p>
-          : (
-            <>
-              <p>{anecdotes[topAnecdoteIndex]}</p>
-              <p>Has {mostVotes} votes</p>
-            </>
-          )
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
         }
-      </div>
-    )
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+        {courses.map((course) => (
+          <Course key={course.id} course={course} />
+        ))}
+    </div>
+  )
 }
 
 export default App
