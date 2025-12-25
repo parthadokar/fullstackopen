c23c909b63f9dca5b9bd9f2f0ac26efdea6156d4
parthadokar <adokar.parth@gmail.com>
2025-12-25 15:29:27 +0530

    1.12-1.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,52 @@
 import { useState } from 'react'
 
-const Button = ({onClick,text}) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
-
-const StatisticLine = ({text,value}) => {
-  return (
-    <p>{text} {value}</p>
-  )
-}
-const Statistics  = ({good,neutral,bad}) => {
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = total === 0 ? 0 : (good / total) * 100
-  if (total === 0) {
-    return (
-      <>
-      <p>Statistics</p>
-      <p>No feedback given</p>
-      </>
-    )
-  }
-  return (
-    <>
-      <p>Statistics</p>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="total" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + ' %'} />
-    </>
-  )
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes,setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const nextAnecdote = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random)
+  }
 
-  const goodOne = () => setGood(good + 1)
-  const neutralOne = () => setNeutral(neutral+1)
-  const badOne = () => setBad(bad+1)
+  const voteAnecdote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const mostVotes = Math.max(...votes)
+  const topAnecdoteIndex = votes.indexOf(mostVotes)
   return (
-    <div>
-      <Button onClick={goodOne} text={good}/>
-      <Button onClick={neutralOne} text={neutral}/>
-      <Button onClick={badOne} text={bad}/>
-      <Statistics 
-        good={good} 
-        neutral={neutral} 
-        bad={bad} 
-      />
-    </div>
-  )
+      <div>
+        <h2>Anecdote of the day</h2>
+        <p>{anecdotes[selected]}</p>
+        <p>Has {votes[selected]} votes</p>
+
+        <button onClick={voteAnecdote}>vote</button>
+        <button onClick={nextAnecdote}>next anecdote</button>
+        <h2>Anecdote with most votes</h2>
+        {mostVotes === 0 
+          ? <p>No votes yet</p>
+          : (
+            <>
+              <p>{anecdotes[topAnecdoteIndex]}</p>
+              <p>Has {mostVotes} votes</p>
+            </>
+          )
+        }
+      </div>
+    )
 }
 
 export default App
