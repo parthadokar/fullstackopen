93e74cc0bee6c454caa4f1cfd1dc41e59289a058
parthadokar <adokar.parth@gmail.com>
2025-12-25 15:15:54 +0530

    1.6-1.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,61 @@
-import { useState } from "react"
-
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+import { useState } from 'react'
 
 const Button = ({onClick,text}) => {
   return (
-    <button onClick={onClick}>
-      {text}
-    </button>
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const StatisticLine = ({text,value}) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+const Statistics  = ({good,neutral,bad}) => {
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive = total === 0 ? 0 : (good / total) * 100
+  if (total === 0) {
+    return (
+      <>
+      <p>Statistics</p>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  return (
+    <>
+      <p>Statistics</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="total" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive + ' %'} />
+    </>
   )
 }
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter+1)
-  const decreasebyOne = () => setCounter(counter-1)
-  const setToZero = () => setCounter(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  const goodOne = () => setGood(good + 1)
+  const neutralOne = () => setNeutral(neutral+1)
+  const badOne = () => setBad(bad+1)
   return (
-    <>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={setToZero} text='zero'/>
-      <Button onClick={decreasebyOne} text='minus'/>
-    </>
+    <div>
+      <Button onClick={goodOne} text={good}/>
+      <Button onClick={neutralOne} text={neutral}/>
+      <Button onClick={badOne} text={bad}/>
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+      />
+    </div>
   )
 }
 
