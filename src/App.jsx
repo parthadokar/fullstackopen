f7e9f69d0bb29973b10cd732610d372297998289
parthadokar <adokar.parth@gmail.com>
2025-12-28 19:10:07 +0530

    2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
   const [filterName,setFilterName] = useState('')
+  
+  useEffect(() =>  {
+  axios.get('http://localhost:3001/persons')
+  .then(response => {
+    console.log('promise completed')
+    setPersons(response.data)
+  })},[])
 
   const addPerson = (event) => {
     event.preventDefault()
     const duplicate = persons.some(person => person.name === newName)
     if (duplicate) {
       alert(`${newName} is already added to phonebook`)
+      return
     }
     const personObject = { name:newName,number:newNumber,id:persons.length+1 }
     setPersons(persons.concat(personObject))
@@ -47,7 +51,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {filteredPersons.map(person =>
-        <p key={person.name}>{person.name} {person.number}</p>
+        <p key={person.id}>{person.name} {person.number}</p>
       )}
       <div>
         debug: {newName}
